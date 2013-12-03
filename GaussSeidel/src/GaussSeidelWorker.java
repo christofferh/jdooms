f6fe5fb4b0c66932a1326d084558d636b87689f6
Daniel Persson <daniel.a.persson@me.com>
2013-12-03 11:22:07 +0100

    Removed comments and unused code.

--- GaussSeidel/src/GaussSeidelWorker.java
+++ GaussSeidel/src/GaussSeidelWorker.java
@@ -4,13 +4,6 @@ import se.uu.it.jdooms.workerdispatcher.DSObject;
 import java.util.Arrays;
 import java.util.Random;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Daniel
- * Date: 25/11/13
- * Time: 15:48
- * To change this template use File | Settings | File Templates.
- */
 public class GaussSeidelWorker implements DSObject{
     DSObjectSpace dsObjectSpace;
 
@@ -19,8 +12,6 @@ public class GaussSeidelWorker implements DSObject{
         //To change body of implemented methods use File | Settings | File Templates.
         this.dsObjectSpace = dsObjectSpace;
     }
-
-
 
     @Override
     public void run() {
@@ -46,19 +37,15 @@ public class GaussSeidelWorker implements DSObject{
 
             System.out.print(printDistribution(distribution));
 
-            int i = 0;
-            int start = 0;
-            int end = 0;
+            int i = 0, start = 0, end = 0;
             for (int columns : distribution) {
                 end += columns;
-                System.out.println("strart: " + start);
-                System.out.println("end: " + end);
                 try {
                     ((Matrix)dsObjectSpace.dsNew("Matrix", i)).Init(copyOfRange(matrix, start, end-1), i, distribution.length);
                 } catch (InstantiationException e) {
-                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                    e.printStackTrace();
                 } catch (IllegalAccessException e) {
-                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                    e.printStackTrace();
                 }
                 start += columns;
                 i++;
@@ -68,48 +55,20 @@ public class GaussSeidelWorker implements DSObject{
 
         int workerID = dsObjectSpace.getWorkerID();
 
-
-        for (int n = 0; n < 1; n++){ // while the tolerance criteria is not met
-            int row = 1;
-            Matrix id = (Matrix)dsObjectSpace.getObject(workerID, DSObjectSpace.Permission.ReadWrite);
-            System.out.println(id);
-            /*if (workerID < 1) { //leftmost
-                Matrix left = (Matrix)dsObjectSpace.dsNew("Matrix", workerID);
-                Matrix right = (Matrix)dsObjectSpace.dsNew("Matrix", workerID + 1);
-
-                for (row = 1; row < left.matrix.length; row+=2) {
-                    int column;
-                    for (column = 1; column < right.matrix[row].length - 1; column+=2) {
-                        left.matrix[row][column] = (left.matrix[row - 1][column] + left.matrix[row + 1][column] + left.matrix[row][column - 1] + left.matrix[row][column + 1])/4;
-                    }
-                    left.matrix[row][column] = (left.matrix[row - 1][column] + left.matrix[row + 1][column] + left.matrix[row][column - 1] + right.matrix[row][0])/4;
-                }
-                for (row = 2; row < left.matrix.length; row+=2) {
-                    int column;
-                    for (column = 1; column < right.matrix[row].length - 1; column+=2) {
-                        left.matrix[row][column] = (left.matrix[row - 1][column] + left.matrix[row + 1][column] + left.matrix[row][column - 1] + left.matrix[row][column + 1])/4;
-                    }
-                    left.matrix[row][column] = (left.matrix[row - 1][column] + left.matrix[row + 1][column] + left.matrix[row][column - 1] + right.matrix[row][0])/4;
-                }
-
+        for (int tolerance = 0; tolerance < 1; tolerance++){ // while the tolerance criteria is not met
+            Matrix id = (Matrix) dsObjectSpace.getObject(workerID, DSObjectSpace.Permission.ReadWrite);
+            printMatrix(id.matrix);
+            if (workerID < 1) { //leftmost
+                Matrix right = (Matrix) dsObjectSpace.getObject(workerID + 1, DSObjectSpace.Permission.Read);
+                id.calculate(null, right);
             } else if (workerID == dsObjectSpace.getWorkerCount() - 1) { //rightmost
-                Matrix left = (Matrix)dsObjectSpace.dsNew("Matrix", workerID - 1);
-                Matrix right = (Matrix)dsObjectSpace.dsNew("Matrix", workerID);
-                int column = 0;
-                right.matrix[row][column] = (right.matrix[row - 1][column] + right.matrix[row + 1][column] + left.matrix[row][left.matrix[row].length] + right.matrix[row][column + 1])/4;
-                for (column = 1; column < right.matrix[row].length; column++) {
-                    right.matrix[row][column] = (right.matrix[row - 1][column] + right.matrix[row + 1][column] + right.matrix[row][column - 1] + right.matrix[row][column + 1])/4;
-                }
-                row++;
+                Matrix left = (Matrix) dsObjectSpace.getObject(workerID - 1, DSObjectSpace.Permission.Read);
+                id.calculate(left, null);
             } else { //in the middle
-                Matrix matN0 = (Matrix)dsObjectSpace.dsNew("Matrix", workerID - 1);
-                Matrix matN1 = (Matrix)dsObjectSpace.dsNew("Matrix", workerID);
-                Matrix matN2 = (Matrix)dsObjectSpace.dsNew("Matrix", workerID + 1);
-                int column = 1;
-                // do work here
-                row++;
-            }*/
-
+                Matrix left = (Matrix) dsObjectSpace.getObject(workerID - 1, DSObjectSpace.Permission.Read);
+                Matrix right = (Matrix) dsObjectSpace.getObject(workerID + 1, DSObjectSpace.Permission.Read);
+                id.calculate(left, right);
+            }
             dsObjectSpace.synchronize();
         }
     }
