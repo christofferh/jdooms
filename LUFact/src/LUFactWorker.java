e2b0df89ae6e3e2c3cbe8c1ae65d995c42ae2799
Daniel Persson <daniel.a.persson@me.com>
2014-01-13 11:11:39 +0100

    Added matrix multiplication to generate matrix.

--- LUFact/src/LUFactWorker.java
+++ LUFact/src/LUFactWorker.java
@@ -197,6 +197,7 @@ public class LUFactWorker implements DSObject{
             }
             n++;
         }
+        AMatrix = multiplyMatrix(AMatrix, AMatrix);
     }
 
     public String printMatrix(float[][] matrix) {
@@ -260,5 +261,32 @@ public class LUFactWorker implements DSObject{
             result[j] = Arrays.copyOfRange(AMatrix[i], column, column + blockSize);
         }
         return result;
+    }
+
+    private float[][] multiplyMatrix(float[][] A, float[][] B) {
+        int aRows = A.length;
+        int aColumns = A[0].length;
+        int bRows = B.length;
+        int bColumns = B[0].length;
+
+        if (aColumns != bRows) {
+            throw new IllegalArgumentException("A:Rows: " + aColumns + " did not match B:Columns " + bRows + ".");
+        }
+
+        float[][] C = new float[aRows][bColumns];
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                C[i][j] = 0;
+            }
+        }
+
+        for (int i = 0; i < aRows; i++) { // aRow
+            for (int j = 0; j < bColumns; j++) { // bColumn
+                for (int k = 0; k < aColumns; k++) { // aColumn
+                    C[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+        return C;
     }
 }
