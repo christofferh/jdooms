0adea53c488a8726d20c2b61d5e83e6e9c8a3426
Daniel Persson <daniel.a.persson@me.com>
2014-01-16 16:54:44 +0100

    Fixed distributed loop

--- GaussSeidel/src/GaussSeidelWorker.java
+++ GaussSeidel/src/GaussSeidelWorker.java
@@ -65,7 +65,9 @@ public class GaussSeidelWorker implements DSObject{
                     }
                     id.calculateRed(left, right);
                 }
-                dsObjectSpace.synchronize();
+            }
+            dsObjectSpace.synchronize();
+            for (int item : workList) {
                 if (item%dsObjectSpace.getWorkerCount() == dsObjectSpace.getWorkerID()) {
                     Matrix id = (Matrix) dsObjectSpace.getObject(item, DSObjectSpace.Permission.ReadWrite);
                     Matrix left = null;
@@ -80,8 +82,8 @@ public class GaussSeidelWorker implements DSObject{
                     }
                     id.calculateBlack(left, right);
                 }
-                dsObjectSpace.synchronize();
             }
+            dsObjectSpace.synchronize();
         }
         final long endTimeCalculate = System.currentTimeMillis();
 
